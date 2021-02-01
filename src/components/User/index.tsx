d05552a9c4780f2e028b17fe59fa237bd19852ed
xande <aleturial8@gmail.com>
2021-02-01 11:38:52 -0300

    user test

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 
-import img1 from '../../assets/dog1.jpg';
+import user1 from '../../assets/user1.png';
 
 import { Container } from './styles';
 
 const User: React.FC = () => {
   return (
     <Container>
-        <img src={img1} alt={img1} />
+        <img src={user1} alt={user1} />
         <div>
             <h3>User</h3>
             <h4>Title post</h4>
